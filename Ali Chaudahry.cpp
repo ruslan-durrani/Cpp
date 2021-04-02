690a439a90929df7f8049c1a85ff7e0db1fe3f49
M-RUSLAN-DURRANI <ruslandurrani907@gmail.com>
2021-04-02 22:37:19 +0500

    Add files via upload

---
+++ Ali Chaudahry.cpp
@@ -0,0 +1,99 @@
+//Program 1
+#include <iostream>
+using namespace std;
+int main()
+ {
+    cout <<"################################### "<< endl;
+	cout <<"$$$$***************************$$$$"<< endl;
+	cout <<"******\\\\Welcome to cust////******"<< endl;
+	cout <<"################################### "<< endl;
+    return 0;
+}
+
+//Program 2
+
+using namespace std;
+int main()
+ {
+    cout <<"*** My Introduction *** "<< endl;
+	cout <<"* Name : Your Name *"<< endl;
+	cout <<"DOB: Your Birthday   *"<< endl;
+	cout <<"*City: Your city  *  *Prog: your Prog  * "<< endl;
+	cout <<"* The End * "<< endl;
+    return 0;
+}
+
+//Program 3
+using namespace std;
+int main()
+ {
+    cout <<"Course code | Course Name          | Credit Hour "<< endl;
+	cout <<"     cs1117 | ITC                  | 4"<< endl;
+	cout <<"      cs134 | Computer Programming | 4"<< endl;
+	cout <<"     cs1123 | Cal-I                | 4 "<< endl;
+	cout <<"     cs1121 | Algebra              | 3 "<< endl;
+	cout <<"     cs1124 | Discrete Maths       | 3 "<< endl;
+	cout <<"     Total  | 5                    | 18 "<< endl;
+    return 0;
+}
+
+//Programs 4
+
+using namespace std;
+int main()
+ {
+    cout <<"   Name   | Ali        "<< endl;
+	cout <<"  Emp_No  | Emp_123                "<< endl;
+	cout <<"   Salary | 30000 "<< endl;
+    return 0;
+}
+
+//Program 5
+using namespace std;
+int main()
+ {
+    cout <<"    Month | Number of hrs         "<< endl;
+	cout <<"    Jan   | 100                 "<< endl;
+	cout <<"    Feb   | 150 "<< endl;
+	cout <<"    March | 130                 "<< endl;
+	cout <<"    April | 90              "<< endl;
+	cout <<"    May   | 110       "<< endl;
+	cout <<"    June  | 135                    "<< endl;
+    return 0;
+}
+
+
+//Program 6
+
+using namespace std;
+int main()
+ {
+    cout <<"    Allowances | Number of hrs         "<< endl;
+	cout <<"    House Rent | 1000                 "<< endl;
+	cout <<"    Medical    | 1500 "<< endl;
+	cout <<"    TA/DA      | 2000                 "<< endl;
+	cout <<"    Bonus      | 50000            "<< endl;
+    return 0;
+}
+
+//Program 7
+
+using namespace std;
+int main()
+ {
+   cout<<'\a'<<endl;
+}
+
+//program 8
+
+
+
+
+
+
+
+
+
+
+
+
